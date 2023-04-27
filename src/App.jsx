63387342934cefe8d5a27176a55f2a3b8bb01ae5
Damian <dsimrayh@yahoo.com>
2023-04-26 19:30:43 -0700

    search functionality added
    built out more UI elements, updated styling, updated API function + logic. added location search feature.

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import './style.css';
 
 function App() {
   const [weather, setWeather] = useState({ location: '' });
+  const [searchValue, setSearchValue] = useState('');
 
-  async function requestWeatherData() {
+  async function requestWeatherData(query) {
     const url = 'https://api.weatherapi.com/v1/forecast.json';
     const key = '3a1962b5b4fa48eab9012505231004';
     try {
-      const response = await fetch(`${url}?key=${key}&q=chicago`);
+      const response = await fetch(`${url}?key=${key}&q=${query}`);
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
       const data = await response.json();
 
       const current = processCurrentWeatherData(data.current);
@@ -26,19 +30,31 @@ function App() {
 
   return (
     <>
-      <button
-        onClick={async () => {
+      <h1>Weather</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          if (!searchValue) return;
           try {
-            const weatherData = await requestWeatherData();
-            setWeather(weatherData);
+            const weatherData = await requestWeatherData(searchValue);
+            if (weatherData) setWeather(weatherData);
           } catch (err) {
             console.error(err);
           }
         }}
       >
-        Get Chicago weather forecast
-      </button>
+        <label htmlFor="search">Enter location:</label>
+        <input
+          type="search"
+          id="search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        ></input>
+        <button type="submit">Search</button>
+      </form>
       {/* Time added as a visual way to confirm state is updating */}
+      <p>{`${weather.location.city}, ${weather.location.region}`}</p>
+      <p>{weather.location.country}</p>
       <p>Local time:</p>
       <p>{weather.location.localtime}</p>
     </>
