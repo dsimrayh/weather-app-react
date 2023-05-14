3011e90a526373958e75911fbf1bedc7f0b91b00
Damian <dsimrayh@yahoo.com>
2023-05-13 23:08:59 -0700

    updated styling for additional weather data

--- src/components/AdditionalWeatherData.jsx
+++ src/components/AdditionalWeatherData.jsx
@@ -3,16 +3,64 @@
 export default function AdditionalWeatherData({ weather }) {
   return (
     <div id="additional-weather-data">
-      <p> Feels like: {weather.current.feelslike_f}º</p>
-      <p> Wind speed: {weather.current.wind_mph} mph</p>
-      <p> Wind direction: {weather.current.wind_dir}</p>
-      <p> Chance of precipitation: {weather.forecast.daily_chance_of_rain}%</p>
-      <p> Total precipitation {weather.forecast.totalprecip_in} in</p>
-      <p> Humidity {weather.current.humidity}%</p>
-      <p> Pressure: {weather.current.pressure_in} in</p>
-      <p> UV: {weather.current.uv}</p>
-      <p> Visibility {weather.current.vis_miles} mi</p>
-      <p> Cloud cover: {weather.current.cloud}%</p>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Feels like</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.feelslike_f}º
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Wind speed</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.wind_mph} mph
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Wind direction</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.wind_dir}
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Precipitation chance</p>
+        <p className="additional-weather-tile-content">
+          {weather.forecast.daily_chance_of_rain}%
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Total precipitation</p>
+        <p className="additional-weather-tile-content">
+          {weather.forecast.totalprecip_in} in
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Humidity</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.humidity}%
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Pressure</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.pressure_in} in
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">UV</p>
+        <p className="additional-weather-tile-content">{weather.current.uv}</p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Visibility</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.vis_miles} mi
+        </p>
+      </div>
+      <div className="additional-weather-tile">
+        <p className="additional-weather-tile-title">Cloud cover</p>
+        <p className="additional-weather-tile-content">
+          {weather.current.cloud}%
+        </p>
+      </div>
     </div>
   );
 }
